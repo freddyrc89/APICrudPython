167d7fe6d36733117dee2e62f0c39c41948795aa
freddyrc89 <freddyrc89@hotmail.com>
2025-03-28 12:30:39 -0500

    puerto a 8000

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ app.register_blueprint(usuario_bp)
 if __name__ == '__main__':
     with app.app_context():
         init_db()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
